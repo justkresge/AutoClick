6a0242890eb52c7ebcfc839f5dbf4eb9ea9f4afc
Justin Kresge <justkresge@gmail.com>
2013-07-16 22:00:43 -0400

    Good night Commit

--- src/com/hooper/autoclick/MainActivity.java
+++ src/com/hooper/autoclick/MainActivity.java
@@ -1,23 +1,63 @@
 package com.hooper.autoclick;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	int Count;
+	Button Add;
+	TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        
+        Count = 0;
+        Add = (Button)findViewById(R.id.button);
+        text = (TextView)findViewById(R.id.text);
+        
+    	
+        
+        Add.setOnClickListener(new View.OnClickListener() {
+        	
+        
+			
+				@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Count ++;
+				text.setText("Count: " + Count);
+				
+				while(Count < 10){
+					 Add.performClick();
+					try {
+		        	    Thread.sleep(1000);
+		        	} catch(InterruptedException ex) {
+		        		
+		        	    Thread.currentThread().interrupt();
+		        	   
+		        	}
+					
+				}
+				}
+		});
+        
+       
+        
+        
+   
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    
+  
+    		
+    	
     }
     
 }
+
